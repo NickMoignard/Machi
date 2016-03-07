9c7d88b2438aa5a1cd8e61285ca54d3f91f999ae
NickMoignard <nicklmoignard@gmail.com>
2016-03-07 18:55:35 +1100

    README.md added

--- Machi/HomeDashboardViewController.swift
+++ Machi/HomeDashboardViewController.swift
@@ -1,53 +1,74 @@
 //
 //  HomeDashboardViewController.swift
 //  Machi
-//
 //  Created by Nicholas Moignard on 7/03/2016.
 //  Copyright © 2016 Nicholas Moignard. All rights reserved.
-//
+
+
+//  The HomeDashboardViewController controls the users Dashboard (The applications main view)
+
+//  MARK:  - To Do Section
+/*
+    Currently the logged in/out logic resides in this view controllers view did apear function. This logic should be moved to the application delegate
+*/
+
+
 
 import UIKit
 
 class HomeDashboardViewController: UIViewController {
     
+    //MARK: - Outlets and Variable Declaration
+    
     var titleText = "Home"
+    
+    
+    
+    
+    //MARK: - View Controller Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = titleText
         
+        //  ~ Load data into view
+        self.title = titleText //Set the Header Title
+        
+        //  ~ Check if user is logged in
         if NSUserDefaults.standardUserDefaults().valueForKey("uid") == nil {
-            // user not logged in
-            // take to login screen
-            performSegueWithIdentifier("loginSegue", sender: self)
+            // ~ user not logged in
+            performSegueWithIdentifier("loginSegue", sender: self) //Take user to login screen
         }
-        
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //  ~ Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    
+    //MARK: - Actions
     
 
     @IBAction func logoutAction(sender: AnyObject) {
-        //log out of firebase
-        CURRENT_USER.unauth()
-        //remove authdata from NSUserDefaults
-        NSUserDefaults.standardUserDefaults().setValue(nil, forKey: "uid")
+        //  ~ Logout Action -
+        //      log out of firebase, remove our authentification data from the local data store and return user to login screen
+        
+        CURRENT_USER.unauth()                                               //log out of firebase
+        NSUserDefaults.standardUserDefaults().setValue(nil, forKey: "uid")  //remove authdata from NSUserDefaults
         print("user logged out")
-        //return to login screen
-        performSegueWithIdentifier("loginSegue", sender: nil)
+        performSegueWithIdentifier("loginSegue", sender: nil)               //return to login screen
     }
-    /*
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
---
+++ Machi/README.md
@@ -0,0 +1,58 @@
+README.md
+
+Machi 
+
+~ The Booking and Payment Solution for Fitness Instructors ~
+
+GENERAL INFORMATION
+
+    Description:
+        iOS Application that allows a user to quickly and easily book a personal training session or fitness class.
+
+        The Service is achivied using a client-server model with a:
+            -   iOS Client for Customers (Swift 2.2)
+            -   iOS Client for Fitness Professionals (Swift 2.2)
+            -   Node.js Sever (Express Router, Firebase noSQL database) 
+
+    Contact Info:
+
+        Developer
+        -   Nicholas Moignard
+        -   email: nicklmoignard@gmail.com
+    
+    3rd party services and librarys:
+
+        -   SwiftJSON   (Elegant JSON Parsing library) 
+            https://github.com/SwiftyJSON/SwiftyJSON
+
+        -   AlamoFire   (HTTP networking library) 
+            https://github.com/Alamofire/Alamofire
+
+        -   Firebase (mBaaS) mobile backend as a service 
+            https://www.firebase.com
+
+        -   Cronofy (API to handle Universal Calendar integration)
+            https://www.cronofy.com
+
+        -   Stripe (Credit Card Payment Service)
+            https://www.stripe.com
+  
+    Supporting files: (Wireframes, Moodboard etc etc)
+
+        <GOOGLE DRIVE SHARING THINGO>
+
+
+GETTING STARTED
+    In order to view and test application
+
+    1.  Install Xcode 7 from the OSX App Store
+    2.  Download current Machi files from GitHub 
+    2.  Open the Machi.xcworkspace file
+    2.  Xcode will open automatically, Click the Build and Run (Play) button  in the top left of the Xcode IDE. 
+    5.  You can change simulated iOS Device with the dropdown next to to build and run button
+
+
+
+
+    
+
